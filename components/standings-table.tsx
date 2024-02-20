6498ed4146814b89e8257457b9e5bd94f5f8fbce
Dan Schafer <dan.schafer@gmail.com>
2024-02-19 20:30:17 -0800

    Just show one league

--- components/standings-table.tsx
+++ components/standings-table.tsx
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 export default async function StandingsTable() {
-  const matches = await prisma.matches.findMany({ take: 10 })
+  const matches = await prisma.matches.findMany({ take: 10, where: { League: "E0" } })
 
   return (
     <TableContainer component={Paper}>
