abb21176fb5b99955e2837a556c88957374a9ba8
Dan Schafer <dan.schafer@gmail.com>
2024-02-21 14:27:42 -0800

    Use stack to improve spacing

--- www/app/(home)/loading.tsx
+++ www/app/(home)/loading.tsx
@@ -1,12 +1,13 @@
+import Stack from "@mui/material/Stack";
 import StandingsTablePlaceholder from "@/components/standings-table-placeholder";
 import leagues from "@/lib/leagues";
 
 export default function Home() {
   return (
-    <>
+    <Stack spacing={2}>
       {Array.from(leagues).map(([league, _]) => (
         <StandingsTablePlaceholder rowCount={5} league={league} key={league} />
       ))}
-    </>
+    </Stack>
   );
 }
--- www/app/(home)/page.tsx
+++ www/app/(home)/page.tsx
@@ -1,9 +1,10 @@
+import Stack from "@mui/material/Stack";
 import StandingsTable from "@/components/standings-table";
 import leagues from "@/lib/leagues";
 
 export default function Home() {
   return (
-    <>
+    <Stack spacing={2}>
       {Array.from(leagues).map(([league, _]) => (
         <StandingsTable
           rowCount={5}
@@ -12,6 +13,6 @@ export default function Home() {
           key={league}
         />
       ))}
-    </>
+    </Stack>
   );
 }
