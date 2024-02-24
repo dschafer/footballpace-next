e85d34451e576484fcac28e24c66743d3d7b7101
Dan Schafer <dan.schafer@gmail.com>
2024-02-23 22:03:31 -0800

    Reduce setup.py to shell

--- data/setup.py
+++ data/setup.py
@@ -1,15 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="footballpace",
-    packages=find_packages(exclude=["footballpace_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "dagster-pandas",
-        "pandas",
-        "psycopg[binary]",
-        "requests",
-    ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
-)
+setup()
