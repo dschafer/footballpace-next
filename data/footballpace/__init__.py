f0b31ce6ef7153d90deef754a0352da4f44f8ce5
Dan Schafer <dan.schafer@gmail.com>
2024-09-28 15:48:41 -0700

    Filter dagster warnings

--- data/footballpace/__init__.py
+++ data/footballpace/__init__.py
@@ -1,14 +1,22 @@
 from dagster import (
     Definitions,
     EnvVar,
+    ExperimentalWarning,
     load_assets_from_package_module,
 )
 
-from . import assets
-from .resources import FootballDataResource, VercelPostgresResource
-from .jobs import cache_update_job, pace_sheets_job, results_job
-from .sensors import db_write_sensor
-from .schedules import current_season_daily_refresh_schedule
+import warnings
+
+warnings.filterwarnings("ignore", category=ExperimentalWarning)
+
+# These are all noqa: E402 so that we can call warnings.filterwarnings
+# above before doing the imports
+from . import assets  # noqa: E402
+from .resources import FootballDataResource, VercelPostgresResource  # noqa: E402
+from .jobs import cache_update_job, pace_sheets_job, results_job  # noqa: E402
+from .sensors import db_write_sensor  # noqa: E402
+from .schedules import current_season_daily_refresh_schedule  # noqa: E402
+
 
 defs = Definitions(
     assets=load_assets_from_package_module(assets),
