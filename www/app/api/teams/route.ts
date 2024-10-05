6d2980f5e1d12839a5a3bc71d85642606c9e3a55
Dan Schafer <dan.schafer@gmail.com>
2024-10-05 09:46:43 -0700

    Add comment explaining route

--- www/app/api/teams/route.ts
+++ www/app/api/teams/route.ts
@@ -1,6 +1,11 @@
 import { NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
 
+// This can't be done with server components because the navlink
+// has to be client (since it has a click handler to hide the menu),
+// the shell has to be client (since it has the burger icon to hide
+// the menu), and this lives in between, so having it be anything
+// other than a client component is awkward.
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const league = searchParams.get("league");
