f8f64aa5865dfd8e5b6012cd4ac669b2c1c8abf5
Dan Schafer <dan.schafer@gmail.com>
2024-10-08 09:53:23 -0700

    Remove sign from 0 on pace

--- www/components/pace-display/pace-number.tsx
+++ www/components/pace-display/pace-number.tsx
@@ -3,7 +3,7 @@ import { NumberFormatter } from "@mantine/core";
 export default function PaceNumber({ pace }: { pace: number }) {
   return (
     <>
-      {pace > 0 ? "+" : "−"}
+      {pace > 0 ? "+" : pace < 0 ? "−" : ""}
       <NumberFormatter
         value={Math.abs(pace)}
         decimalScale={2}
