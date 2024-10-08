86eaec1ab74d8c51dc77785282640afba5232abb
Dan Schafer <dan.schafer@gmail.com>
2024-10-08 14:52:49 -0700

    Fix dark mode colored cell

--- www/components/pace-display/colored-cell.tsx
+++ www/components/pace-display/colored-cell.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { TableTd, TableTdProps, isLightColor } from "@mantine/core";
+import {
+  TableTd,
+  TableTdProps,
+  isLightColor,
+  useComputedColorScheme,
+} from "@mantine/core";
 
 export default function ColoredCell(
   props: {
@@ -13,22 +18,43 @@ export default function ColoredCell(
     );
   }
 
-  // TODO: Get this working for dark mode
-  const bgColors = [
-    "#762a83",
-    "#af8dc3",
-    "#e7d4e8",
-    "#f7f7f7",
-    "#d9f0d3",
-    "#7fbf7b",
-    "#1b7837",
-  ];
-  const bg = (delta: number) => bgColors[Math.floor(delta + 3.5)];
-  const fg = (delta: number) => (isLightColor(bg(delta)) ? "black" : "white");
+  const computedColorScheme = useComputedColorScheme("light");
 
-  return (
-    <TableTd p="0" bg={bg(props.val)} c={fg(props.val)} {...props}>
-      {props.children}
-    </TableTd>
-  );
+  if (computedColorScheme == "light") {
+    const bgColors = [
+      "#762a83",
+      "#af8dc3",
+      "#e7d4e8",
+      "#f7f7f7",
+      "#d9f0d3",
+      "#7fbf7b",
+      "#1b7837",
+    ];
+    const bg = (delta: number) => bgColors[Math.floor(delta + 3.5)];
+    const fg = (delta: number) => (isLightColor(bg(delta)) ? "black" : "white");
+
+    return (
+      <TableTd p="0" bg={bg(props.val)} c={fg(props.val)} {...props}>
+        {props.children}
+      </TableTd>
+    );
+  } else {
+    const bgColors = [
+      "#762a83",
+      "#5b2863",
+      "#3f2644",
+      "#242424",
+      "#21402a",
+      "#1e5d31",
+      "#1b7837",
+    ];
+    const bg = (delta: number) => bgColors[Math.floor(delta + 3.5)];
+    const fg = (delta: number) => (isLightColor(bg(delta)) ? "black" : "white");
+
+    return (
+      <TableTd p="0" bg={bg(props.val)} c={fg(props.val)} {...props}>
+        {props.children}
+      </TableTd>
+    );
+  }
 }
