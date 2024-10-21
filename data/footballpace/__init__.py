d2907514faf1fb34e10b25ba7afae6b71e157c57
Dan Schafer <dan.schafer@gmail.com>
2024-10-21 14:03:42 -0700

    Silence warnings as early as possible

--- data/footballpace/__init__.py
+++ data/footballpace/__init__.py
@@ -1,24 +1,28 @@
-from dagster import (
-    Definitions,
-    EnvVar,
-    ExperimentalWarning,
-    load_assets_from_package_module,
-)
+from dagster import ExperimentalWarning
 
 import warnings
 
-from footballpace.resources.http import HTTPResource
-
 warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
-# These are all noqa: E402 so that we can call warnings.filterwarnings
+# This file is all noqa: E402 so that we can call warnings.filterwarnings
 # above before doing the imports
-from . import assets  # noqa: E402
-from .resources.footballdata import FootballDataResource  # noqa: E402
-from .resources.vercel import VercelPostgresResource  # noqa: E402
-from .jobs import cache_update_job, fpl_job, pace_sheets_job, results_job  # noqa: E402
-from .sensors import db_write_sensor  # noqa: E402
-from .schedules import (  # noqa: E402
+# ruff: noqa: E402
+
+from dagster import (
+    Definitions,
+    EnvVar,
+    load_assets_from_package_module,
+)
+
+from footballpace.resources.http import HTTPResource
+
+
+from . import assets
+from .resources.footballdata import FootballDataResource
+from .resources.vercel import VercelPostgresResource
+from .jobs import cache_update_job, fpl_job, pace_sheets_job, results_job
+from .sensors import db_write_sensor
+from .schedules import (
     current_season_refresh_schedule,
     fpl_refresh_schedule,
     pace_sheets_refresh_schedule,
