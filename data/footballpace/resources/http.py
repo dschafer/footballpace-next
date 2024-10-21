71015f2c182a420c98540a1b519f6dc98d26ced8
Dan Schafer <dan.schafer@gmail.com>
2024-10-21 13:20:24 -0700

    Use requests session in HTTP resource

--- data/footballpace/resources/http.py
+++ data/footballpace/resources/http.py
@@ -1,12 +1,22 @@
+from contextlib import contextmanager
+from pydantic import PrivateAttr
 import requests
-from dagster import ConfigurableResource
+from dagster import ConfigurableResource, InitResourceContext
 
 
 class HTTPResource(ConfigurableResource):
     """Resource to fetch data from the internet."""
 
+    _request_session: requests.Session = PrivateAttr()
+
+    @contextmanager
+    def yield_for_execution(self, context: InitResourceContext):
+        with requests.Session() as s:
+            self._request_session = s
+            yield self
+
     def get(self, url) -> requests.Response:
         """Get the provided URL."""
-        response = requests.get(url)
+        response = self._request_session.get(url)
         response.raise_for_status()
         return response
