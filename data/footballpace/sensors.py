2182289520795fe1074dd9c3c214b35d0759ee0f
Dan Schafer <dan.schafer@gmail.com>
2024-10-26 15:18:47 -0700

    Update cache_update sensor to not look at row counts now that we short circuit

--- data/footballpace/sensors.py
+++ data/footballpace/sensors.py
@@ -1,33 +1,13 @@
 from typing import Optional
 from dagster import (
-    AssetMaterialization,
     AssetSelection,
     DefaultSensorStatus,
-    IntMetadataValue,
     MultiAssetSensorEvaluationContext,
     RunRequest,
     multi_asset_sensor,
 )
 
 from .jobs import cache_update_job
-
-
-def metadata_int(m: AssetMaterialization, key: str) -> int:
-    metadata = m.metadata
-    val = metadata.get(key)
-    if not val:
-        return 0
-    if not isinstance(val, IntMetadataValue):
-        return 0
-    if not val.value:
-        return 0
-    return val.value
-
-
-def row_count(m: AssetMaterialization) -> int:
-    return metadata_int(m, "dagster/partition_row_count") + metadata_int(
-        m, "dagster/row_count"
-    )
 
 
 @multi_asset_sensor(
@@ -41,23 +21,12 @@ def db_write_sensor(
     """
     This sensor listens to DB writes, and will invalidate the Vercel cache when they happen.
 
-    For this to trigger, two things must be true for an asset:
+    For this to trigger, an asset must set the tag "db_write" to "true".
 
-    First, it must set the tag "db_write" to "true".
-    Second, it must include either "dagster/partition_row_count" or "dagster/row_count" in
-      its metadata, with the number of rows that is changed.
-
-    The sensor will then only fire when it sees changed rows, to avoid invalidating the cache unnecessarily.
+    The sensor will then only fire when it sees those assets materialize, to avoid invalidating
+    the cache unnecessarily.
     """
     asset_events = context.latest_materialization_records_by_key()
-    event_logs = [el for el in asset_events.values() if el is not None]
-    if len(event_logs) == 0:
-        return
-    context.log.info("db_write_sensor saw %d events", len(event_logs))
-    materializations = [el.asset_materialization for el in event_logs]
-    context.log.info("db_write_sensor saw %d materializations", len(materializations))
-    total_rows_written = sum([row_count(m) for m in materializations if m is not None])
-    context.log.info("db_write_sensor saw %d rows written", total_rows_written)
-    if total_rows_written > 0:
+    if any(asset_events.values()):
         context.advance_all_cursors()
         return RunRequest()
