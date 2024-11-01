856daa516bd9f39f5a8824ef0476e8eca2484619
Dan Schafer <dan.schafer@gmail.com>
2024-11-01 13:14:10 -0700

    Fix status check in update script

--- data/docker/update.sh
+++ data/docker/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git remote update
-if [ $(git status -uno --porcelain | wc -l) -gt 0 ]; then
+if [ $(git status | grep behind |  wc -l) -gt 0 ]; then
   git pull
   docker compose --env-file ../.env up --detach --build dagster-footballpace-data
 fi
