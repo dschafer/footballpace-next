020b73a33561c1758b08efa709f7a2ea142d1f2b
Dan Schafer <dan.schafer@gmail.com>
2024-11-17 14:06:30 -0800

    Improve color of fixture difficulty

--- www/components/pace-display/use-fixture-difficulty-color.ts
+++ www/components/pace-display/use-fixture-difficulty-color.ts
@@ -1,5 +1,10 @@
 "use client";
-import { isLightColor, useComputedColorScheme } from "@mantine/core";
+import {
+  darken,
+  isLightColor,
+  lighten,
+  useComputedColorScheme,
+} from "@mantine/core";
 
 export default function useFixtureDifficultyColor(
   fixtureDifficulty: number,
@@ -10,44 +15,24 @@ export default function useFixtureDifficultyColor(
     );
   }
   const computedColorScheme = useComputedColorScheme("light");
-  // https://github.com/jthomasmock/gtExtras/blob/HEAD/R/gt_hulk_color.R
-  // https://colordesigner.io/gradient-generator/?mode=srgb#762A83-F7F7F7
-  // https://colordesigner.io/gradient-generator/?mode=srgb#F7F7F7-1b7837
+  // https://colorbrewer2.org/?type=diverging&scheme=RdYlGn&n=11
   let bgColors = [
-    "#762a83",
-    "#8c4c96",
-    "#a16eaa",
-    "#b791bd",
-    "#ccb3d0",
-    "#e2d5e4",
-    "#f7f7f7",
-    "#d2e2d7",
-    "#aecdb7",
-    "#89b897",
-    "#64a377",
-    "#408e57",
-    "#1b7937",
+    "#a50026",
+    "#d73027",
+    "#f46d43",
+    "#fdae61",
+    "#fee08b",
+    "#ffffbf",
+    "#d9ef8b",
+    "#a6d96a",
+    "#66bd63",
+    "#1a9850",
+    "#006837",
   ];
-
   if (computedColorScheme == "dark") {
-    // https://github.com/jthomasmock/gtExtras/blob/HEAD/R/gt_hulk_color.R
-    // https://colordesigner.io/gradient-generator/?mode=srgb#762A83-242424
-    // https://colordesigner.io/gradient-generator/?mode=srgb#242424-1b7837
-    bgColors = [
-      "#762a83",
-      "#682973",
-      "#5b2863",
-      "#4d2753",
-      "#3f2644",
-      "#322534",
-      "#242424",
-      "#233227",
-      "#21402a",
-      "#204f2e",
-      "#1e5d31",
-      "#1c6b34",
-      "#1b7837",
-    ];
+    bgColors = bgColors.map((x) => darken(x, 0.25));
+  } else {
+    bgColors = bgColors.map((x) => lighten(x, 0.25));
   }
 
   const bg =
