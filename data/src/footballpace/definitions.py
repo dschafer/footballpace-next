de6f98eb19873ddd23b740ddbc874599b06d4bcb
Dan Schafer <dan.schafer@gmail.com>
2026-05-07 20:00:32 -0700

    Modernize Dagster definitions loading

--- data/src/footballpace/definitions.py
+++ data/src/footballpace/definitions.py
@@ -3,28 +3,26 @@ import warnings
 
 import dagster as dg
 
-warnings.filterwarnings("ignore", category=dg.BetaWarning)
-
 
 def augment_assets_with_references(asset_defs):
     if asset_defs is None:
         return None
-    return dg.link_code_references_to_git(
-        dg.with_source_code_references(asset_defs),
-        git_url="https://github.com/dschafer/footballpace-next",
-        git_branch="main",
-        file_path_mapping=dg.AnchorBasedFilePathMapping(
-            local_file_anchor=Path(__file__),
-            file_anchor_path_in_repository="data/src/footballpace/definitions.py",
-        ),
-    )
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", dg.BetaWarning)
+        return dg.link_code_references_to_git(
+            dg.with_source_code_references(asset_defs),
+            git_url="https://github.com/dschafer/footballpace-next",
+            git_branch="main",
+            file_path_mapping=dg.AnchorBasedFilePathMapping(
+                local_file_anchor=Path(__file__),
+                file_anchor_path_in_repository="data/src/footballpace/definitions.py",
+            ),
+        )
 
 
 @dg.definitions
 def defs() -> dg.Definitions:
-    folder_defs = dg.load_from_defs_folder(
-        project_root=Path(__file__).parent.parent.parent
-    )
+    folder_defs = dg.load_from_defs_folder(path_within_project=Path(__file__))
     return dg.Definitions(
         assets=augment_assets_with_references(folder_defs.assets),
         asset_checks=folder_defs.asset_checks,
