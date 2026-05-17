3a5f40ce0000f37cc96ea123d33d809a30378d25
Dan Schafer <dan.schafer@gmail.com>
2026-05-16 20:21:24 -0700

    Add local suspense to pace views

--- www/components/pace-chart/standings-pace-chart.tsx
+++ www/components/pace-chart/standings-pace-chart.tsx
@@ -1,17 +1,34 @@
 import PaceChart from "./pace-chart";
+import PaceChartPlaceholder from "./pace-chart-placeholder";
+import { Suspense } from "react";
 import { fetchPaceTeams } from "@/lib/pace/pace";
 import { fetchTeamColorMap } from "@/lib/color";
+import { shouldCachePaceData } from "@/lib/pace/data";
 import { slicePaceTeams } from "@/lib/pace/pace-types";
 
-export default async function StandingsPaceChart({
-  league,
-  year,
-  targetFinish = 1,
-}: {
+type StandingsPaceChartProps = {
   league: string;
   year: number;
   targetFinish?: number;
-}) {
+};
+
+export default function StandingsPaceChart(props: StandingsPaceChartProps) {
+  const targetFinish = props.targetFinish ?? 1;
+  if (shouldCachePaceData(props.league, props.year, targetFinish)) {
+    return <StandingsPaceChartContent {...props} />;
+  }
+  return (
+    <Suspense fallback={<PaceChartPlaceholder />}>
+      <StandingsPaceChartContent {...props} />
+    </Suspense>
+  );
+}
+
+async function StandingsPaceChartContent({
+  league,
+  year,
+  targetFinish = 1,
+}: StandingsPaceChartProps) {
   const [paceTeams, teamColorMap] = await Promise.all([
     fetchPaceTeams(league, year, targetFinish),
     fetchTeamColorMap(),
--- www/components/pace-table/standings-pace-table.tsx
+++ www/components/pace-table/standings-pace-table.tsx
@@ -1,18 +1,41 @@
 import PaceTable from "./pace-table";
+import PaceTablePlaceholder from "./pace-table-placeholder";
+import { Suspense } from "react";
 import { fetchPaceTeams } from "@/lib/pace/pace";
+import { shouldCachePaceData } from "@/lib/pace/data";
 import { slicePaceTeams } from "@/lib/pace/pace-types";
 
-export default async function StandingsPaceTable({
-  rowCount,
-  league,
-  year,
-  targetFinish,
-}: {
+type StandingsPaceTableProps = {
   rowCount?: number;
   league: string;
   year: number;
   targetFinish: number;
-}) {
+};
+
+export default function StandingsPaceTable(props: StandingsPaceTableProps) {
+  if (shouldCachePaceData(props.league, props.year, props.targetFinish)) {
+    return <StandingsPaceTableContent {...props} />;
+  }
+  return (
+    <Suspense
+      fallback={
+        <PaceTablePlaceholder
+          rowCount={props.rowCount ?? 20}
+          matchdayCount={20}
+        />
+      }
+    >
+      <StandingsPaceTableContent {...props} />
+    </Suspense>
+  );
+}
+
+async function StandingsPaceTableContent({
+  rowCount,
+  league,
+  year,
+  targetFinish,
+}: StandingsPaceTableProps) {
   let paceTeams = await fetchPaceTeams(league, year, targetFinish);
   if (rowCount) {
     paceTeams = slicePaceTeams(paceTeams, rowCount, targetFinish);
